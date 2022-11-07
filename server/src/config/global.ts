e427c6f775f37bbc25b1588580b3c195c44f8ac9
v_jiechliu <v_jiechliu@tencent.com>
2022-11-07 16:17:03 +0800

    feat: 密码

--- server/src/config/global.ts
+++ server/src/config/global.ts
@@ -2,6 +2,6 @@ const { HOST, PORT } = process.env;
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('global', () => ({
-  host: HOST || '127.0.0.1',
+  host: HOST || '0.0.0.0',
   port: PORT || '8080',
 }));
